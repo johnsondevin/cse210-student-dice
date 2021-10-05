39eaa406ba87d52ea0280ecfd0990729bcfd7363
johnsondevin <91571848+johnsondevin@users.noreply.github.com>
2021-10-05 16:34:44 -0600

    Update thrower.py

--- dice/game/thrower.py
+++ dice/game/thrower.py
@@ -1,19 +1,30 @@
 import random
 
-# TODO: Define the Thrower class here.
-
 class Thrower:
-    """
-    
+    """Code that allows for the director of the game to roll the dice.
+
+    Attributes:
+        
+
     """
     def can_throw():
         """
         """
-    
+
     def get_points():
+        """Code that adds the rolled numbers together and returns that number
+        to the function call.
         """
-        """
+        
+
+        return 
 
     def throw_dice():
+        """Code that rolls the dice for the player.
         """
-        """
+        get_numbers = []
+        for _ in range(9):
+            get_numbers.append = random.randint(1,9)
+
+
+        
