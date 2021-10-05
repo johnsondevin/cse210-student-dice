6b7fb353a611a15a5746ca0755445da9c3e2033a
johnsondevin <91571848+johnsondevin@users.noreply.github.com>
2021-10-05 15:52:04 -0600

    Update thrower.py

--- dice/game/thrower.py
+++ dice/game/thrower.py
@@ -1,3 +1,19 @@
 import random
 
 # TODO: Define the Thrower class here.
+
+class Thrower:
+    """
+    
+    """
+    def can_throw():
+        """
+        """
+    
+    def get_points():
+        """
+        """
+
+    def throw_dice():
+        """
+        """
