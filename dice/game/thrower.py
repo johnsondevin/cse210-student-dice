c666a6c726a7fd4723958f4e9103a5a09b42db9c
johnsondevin <91571848+johnsondevin@users.noreply.github.com>
2021-10-06 17:59:37 -0600

    Update thrower.py

--- dice/game/thrower.py
+++ dice/game/thrower.py
@@ -4,27 +4,61 @@ class Thrower:
     """Code that allows for the director of the game to roll the dice.
 
     Attributes:
-        
-
+        dice: The list of numbers that the player rolls
+        num_throws: A counter of how many times the player rolls each game  
     """
-    def can_throw():
-        """
-        """
 
-    def get_points():
+    def __init__(self):
+        """The class constructor
+
+        Arguments:
+            self (Thrower): An instance of Thrower
+        """
+        self.dice = []
+        self.num_throws = 0
+
+    def can_throw(self):
+        """Determines if the player rolled a 1 or a 5, or if the number of rolls
+        is greater than 0 and returns a boolean value to the director.do_outputs
+        function.
+
+        Arguments:
+            self (Thrower): An instance of Thrower
+        """
+        for num in self.dice:
+            if num == 1 or num == 5 or self.num_throws > 0:
+                valid_player = True
+            else:
+                valid_player = False
+
+        return valid_player        
+
+    def get_points(self):
         """Code that adds the rolled numbers together and returns that number
         to the function call.
+
+        Arguments:
+            self (Thrower): An instance of Thrower
         """
-        
+        points = 0
 
-        return 
+        for i in self.dice:
+            if i == 1:
+                points += 100
+            elif i == 5:
+                points += 50
 
-    def throw_dice():
+        return points
+
+    def throw_dice(self):
         """Code that rolls the dice for the player.
+
+        Arguments:
+            self (Thrower): An instance of Thrower
         """
-        get_numbers = []
-        for _ in range(9):
-            get_numbers.append = random.randint(1,9)
-
-
-        
+        for i in range(6):
+            if self.num_throws == 0:
+                self.dice.append(random.randint(1,6))
+            else:
+                self.dice[i] = random.randint(1,6)
+        self.num_throws += 1
